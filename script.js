0595824d0b2c3168de29f5e62ac1c1f3f6dcaa40
Laine Hallot <lainehallot@gmail.com>
2025-02-07 22:36:54 -0600

    refactor: switch to arrow functions because i like them

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let attempts = 0;
 let statusTimer = null;
 let slidersFrozen = false;
 
-function init() {
+const init = () => {
   hInput = document.getElementById("hin");
   sInput = document.getElementById("sin");
   lInput = document.getElementById("lin");
@@ -36,29 +36,29 @@ function init() {
   restart.addEventListener("click", newGame);
   mode.addEventListener("click", changeMode);
   newGame();
-}
+};
 
-function newGame() {
+const newGame = () => {
   pickHSL();
   setupGame(h, s, l);
-}
+};
 
-function setGame(hh, ss, ll) {
+const setGame = (hh, ss, ll) => {
   // eslint-disable-line no-unused-vars
   // Useful to set arbitrary colors for local testing.
   h = hh;
   s = ss;
   l = ll;
   setupGame(h, s, l);
-}
+};
 
-function pickHSL() {
+const pickHSL = () => {
   h = Math.floor(Math.random() * 20) * 18;
   s = Math.floor(Math.random() * 20) * 5;
   l = Math.floor(Math.random() * 20) * 5;
-}
+};
 
-function setupGame(h, s, l) {
+const setupGame = (h, s, l) => {
   log("setting up new game");
   info.style.display = "inline-block";
   submit.style.display = "none";
@@ -72,9 +72,9 @@ function setupGame(h, s, l) {
   paintGame(h, s, l);
   mode.href = window.location.hash === "#x" ? "#" : "#x";
   log("new game is ready");
-}
+};
 
-function paintGame(h, s, l) {
+const paintGame = (h, s, l) => {
   const dark = luminance(h, s, l) < luminance(0, 0, 34.5);
   document.body.style.background = toCssHsl(h, s, l);
   document.body.style.color = dark ? "#fff" : "#000";
@@ -83,9 +83,9 @@ function paintGame(h, s, l) {
     const a = anchors[i];
     a.style.color = dark ? "#eef" : "#006";
   }
-}
+};
 
-function readInput(e) {
+const readInput = (e) => {
   if (slidersFrozen) {
     hInput.valueAsNumber = hh;
     sInput.valueAsNumber = ss;
@@ -98,13 +98,13 @@ function readInput(e) {
     submit.style.display = "inline-block";
     info.style.display = "none";
   }
-}
+};
 
-function submitInput() {
+const submitInput = () => {
   addMove(h, s, l, hh, ss, ll, ++attempts);
-}
+};
 
-function addMove(h, s, l, hh, ss, ll, attempts) {
+const addMove = (h, s, l, hh, ss, ll, attempts) => {
   const dark = luminance(hh, ss, ll) < luminance(8, 8, 8);
   const hslValue = toCssHsl(hh, ss, ll);
   const won = hh === h && ss === s && ll === l;
@@ -143,23 +143,23 @@ function addMove(h, s, l, hh, ss, ll, attempts) {
   if (attempts === MAX_ATTEMPTS) {
     halt();
   }
-}
+};
 
-function halt() {
+const halt = () => {
   hInput.disabled = sInput.disabled = lInput.disabled = true;
   submit.style.display = "none";
   restart.style.display = "inline-block";
   log("halt complete");
-}
+};
 
-function win() {
+const win = () => {
   slidersFrozen = true;
   submit.style.display = "none";
   restart.style.display = "inline-block";
   log("win complete");
-}
+};
 
-function score(h, s, l, hh, ss, ll) {
+const score = (h, s, l, hh, ss, ll) => {
   const maxHErr = Math.max(h, 360 - h);
   const maxSErr = Math.max(s, 100 - s);
   const maxLErr = Math.max(l, 100 - l);
@@ -171,17 +171,17 @@ function score(h, s, l, hh, ss, ll) {
   const lErr = Math.abs(ll - l);
   const dist = Math.sqrt(hErr * hErr + sErr * sErr + lErr * lErr);
   return Math.floor(100 * (1 - dist / maxDist));
-}
+};
 
-function toCssHsl(h, s, l) {
+const toCssHsl = (h, s, l) => {
   return `hsl(${h} ${s}% ${l}%)`.toUpperCase();
-}
+};
 
-function luminance(h, s, l) {
+const luminance = (h, s, l) => {
   return l; // not technically correct but should be good enough to determine with text color to use
-}
+};
 
-function changeMode(e) {
+const changeMode = (e) => {
   e.preventDefault();
   if (window.location.hash === "#x") {
     window.history.replaceState(null, "", "#");
@@ -192,9 +192,9 @@ function changeMode(e) {
     mode.href = "#";
     showStatus("Switched to expert mode!");
   }
-}
+};
 
-function showStatus(msg) {
+const showStatus = (msg) => {
   let opacity = 100;
   let subtract = 0.01;
 
@@ -206,7 +206,7 @@ function showStatus(msg) {
     clearTimeout(statusTimer);
   }
 
-  statusTimer = setInterval(function () {
+  statusTimer = setInterval(() => {
     opacity -= subtract;
     subtract *= 1.2;
     if (statusTimer != null && opacity <= 0) {
@@ -216,9 +216,9 @@ function showStatus(msg) {
     }
     status.style.opacity = opacity + "%";
   }, 50);
-}
+};
 
-function log() {
+const log = () => {
   const args = Array.prototype.slice.call(arguments);
   if (LOGGING) {
     console.log(
@@ -236,6 +236,6 @@ function log() {
         args.join(" ")
     );
   }
-}
+};
 
 window.addEventListener("load", init);
